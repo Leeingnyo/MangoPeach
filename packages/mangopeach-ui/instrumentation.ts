dd37c69ef303fccc78dd3680411f82f11befb9f2
Lee Inyong <dyd1928@naver.com>
2025-07-14 09:10:08 +0900

    Remove server feature in next.js

--- packages/mangopeach-ui/instrumentation.ts
+++
@@ -1,7 +0,0 @@
-export async function register() {
-  if (process.env.NEXT_RUNTIME === 'nodejs') {
-    console.log('Starting server instrumentation, ensuring core is initialized...');
-    // By importing the core module, we trigger the singleton initialization.
-    await import('@/lib/core');
-  }
-}
